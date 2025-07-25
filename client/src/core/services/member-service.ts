466deece469a38995a6a255cbcdff1fecb9b73d6
Arjen van Klaveren <arjenvklaveren@gmail.com>
2025-07-25 14:44:09 +0200

    Photo uploading (Section 11)

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -29,7 +29,22 @@ export class MemberService {
   getMemberPhotos(id: string) {
     return this.http.get<Photo[]>(this.baseUrl + 'members/' + id + '/photos');
   }
+
   updateMember(member: EditableMember) {
     return this.http.put(this.baseUrl + 'members', member);
+  }
+
+  uploadPhoto(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post<Photo>(this.baseUrl + 'members/add-photo', formData);
+  }
+
+  setMainPhoto(photo: Photo) {
+    return this.http.put(this.baseUrl + 'members/set-main-photo/' + photo.id, {});
+  }
+
+  deletePhoto(photoId: number){
+    return this.http.delete(this.baseUrl + 'members/delete-photo/' + photoId);
   }
 }
