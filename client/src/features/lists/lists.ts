1937306a6eb877957d3333471eb5de4b7f96a014
Arjen van Klaveren <arjenvklaveren@gmail.com>
2025-07-27 15:50:10 +0200

    Liking users functionality (Section 14)

--- client/src/features/lists/lists.ts
+++ client/src/features/lists/lists.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { LikesService } from '../../core/services/likes-service';
+import { Member } from '../../types/member';
+import { MemberCard } from "../members/member-card/member-card";
+import { PaginatedResult } from '../../types/pagination';
+import { Paginator } from "../../shared/paginator/paginator";
 
 @Component({
   selector: 'app-lists',
-  imports: [],
+  imports: [MemberCard, Paginator],
   templateUrl: './lists.html',
   styleUrl: './lists.css'
 })
-export class Lists {
+export class Lists implements OnInit{
+  private likesService = inject(LikesService);
+  protected paginatedMembers = signal<PaginatedResult<Member> | null>(null);
+  protected predicate = 'liked';
+  protected pageSize = 5;
+  protected pageNumber = 1;
 
+  tabs = [
+    {label: 'Liked', value: 'liked'},
+    {label: 'Liked me', value: 'likedBy'},
+    {label: 'Mutual', value: 'mutual'},
+  ]
+
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  setPredicate(predicate: string) {
+    if (this.predicate !== predicate) {
+      this.predicate = predicate;
+      this.pageNumber = 1;
+      this.loadLikes();
+    }
+  }
+
+  loadLikes() {
+    this.likesService.getLikes(this.predicate, this.pageNumber, this.pageSize).subscribe({
+      next: members => {
+        this.paginatedMembers.set(members);
+      }
+    })
+  }
+
+  onPageChange(event: {pageNumber: number, pageSize: number}) {
+    this.pageSize = event.pageSize;
+    this.pageNumber = event.pageNumber;
+    this.loadLikes();
+  }
 }
--- client/src/types/member.ts
+++ client/src/types/member.ts
@@ -26,3 +26,4 @@ export class MemberParams {
   pageSize = 10;
   orderBy = 'lastActive';
 }
+
